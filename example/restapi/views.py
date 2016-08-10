ac63986702a6b7284c93080ed61e7016390a5046
tony.zhang <tony.zhang@netis.com.cn>
2016-08-10 16:27:37 +0800

    init demo

---
+++ example/restapi/views.py
@@ -0,0 +1,9 @@
+from serializer import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+
+
+class UserViewSet(viewsets.ModelViewSet):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
